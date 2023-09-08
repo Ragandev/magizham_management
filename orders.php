5d65e0ff507fab8ceb1fb9bfab26cba0ca98de4c
Ragandev <ranjith44.hhh@gmail.com>
2023-09-08 15:39:02 +0530

    Revert "Merge branch 'main' into branch23"
    This reverts commit 440001dcd25071e9d0c7afea8a61e9e9b56570af, reversing
    changes made to 1527b6982024944a6d3f81ec34933a3ecc19e613.

--- orders.php
+++ orders.php
@@ -25,23 +25,41 @@ $logUser = $_SESSION['user'];
         </a>
     </div>
     <?php if (!empty($_GET['succ'])): ?>
-					  
-                      <div class="alert alert-success alert-dismissible fade show" role="alert">
-                    <strong><?php  echo $_GET['succ'] ?></strong>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>
-                                        <?php endif ?>
-                                        <?php if (!empty($_GET['err'])): ?>
-                                          <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                    <strong><?php  echo $_GET['err'] ?></strong>
-                    <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                      <span aria-hidden="true">&times;</span>
-                    </button>
-                  </div>  
-                                        <?php endif ?>
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong><?php  echo $_GET['succ'] ?></strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif ?>
+    <?php if (!empty($_GET['err'])): ?>
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong><?php  echo $_GET['err'] ?></strong>
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    <?php endif ?>
     <h2 class="mb-3">Orders</h2>
+
+
+    <!-- Table for Orders -->
+    <table id="orderTable" class="table table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Branch</th>
+                <th>Order Date</th>
+                <th>Delivery Date</th>
+                <th>Priority</th>
+                <th>Status</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            $orderSql = "SELECT * FROM `order`";
+            $orderData = $pdo->query($orderSql);
 
     <?php
 
@@ -93,13 +111,41 @@ $logUser = $_SESSION['user'];
     }
     ?>
 
+            if ($orderData) {
+                foreach ($orderData as $row) {
+                    $branchee = $pdo->query('SELECT name FROM `branch` WHERE id="' . $row["branchid"] . '"');
+                    $branchee = $branchee->fetch(PDO::FETCH_ASSOC);
+                    echo "<tr>";
+                    echo "<td>" . $row['id'] . "</td>";
+                    echo "<td>" . $branchee['name'] . "</td>";
+                    echo "<td>" . $row['orderdate'] . "</td>";
+                    echo "<td>" . $row['deliverydate'] . "</td>";
+                    echo "<td>" . $row['priority'] . "</td>";
+                    echo "<td>" . $row['status'] . "</td>";
+                    echo "<td>
+                        <a href='edit-order.php?id=" . $row['id'] . "'><i class=' typcn typcn-edit'></i></a> | 
+                        <a href='delete-order.php?delete_id=" . $row['id'] . "' class='text-danger' onclick='return confirmDelete()'><i class=' typcn typcn-trash'></i></a>
+                        <a href='view-order.php?id=" . $row['id'] . "'><i class='typcn typcn-eye'></i></a>
+                    </td>";
+                    echo "</tr>";
+                }
+            } else {
+                echo "Error fetching data";
+            }
+            ?>
+        </tbody>
+    </table>
 </div>
 
 <?php
 include('footer.php');
 ?>
 <script>
-function confirmDelete() {
-    return confirm("Are you sure you want to delete this order?");
-}
+    // $(document).ready(function() {
+    //     $('#orderTable').DataTable();
+    // });
+
+    function confirmDelete() {
+        return confirm("Are you sure you want to delete this order?");
+    }
 </script>
--- role.php
+++ role.php
@@ -10,18 +10,17 @@ require('db.php');
 
 ?>
 
-
 <div class="container">
     <h3>Roles & Permission</h3><br>
     <div class="main-box">
         <table class="table table-bordered table-responsive-lg table-striped">
             <form action="role_post.php" method="POST">
                 <label for="role_id">Role Name</label>
-                <input type="text" id="role_id" class="form-control" name="role_name"><br>
-                
+                <input type="text" id="role_id" class="form-control" name="role_name" require><br>
             <thead>
             <tr>
                 <th>Role</th>
+                <th>Access</th>
                 <th>Create</th>
                 <th>View</th>
                 <th>Edit</th>
@@ -30,35 +29,90 @@ require('db.php');
             </thead>
             <tbody>
                 <tr>
-                    <td>Order</td>
-                    <td><input value="1" type="checkbox" name="view_order"></td>
-                    <td><input value="1" type="checkbox" name="edit_order"></td>
-                    <td><input value="1" type="checkbox" name="create_order"></td>
-                    <td><input value="1" type="checkbox" name="delete_order"></td>
+                    <td>Food Order</td>
+                    <td><select class="form-control" name="fo_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="view_fo"></td>
+                    <td><input value="2" type="checkbox" name="edit_fo"></td>
+                    <td><input value="3" type="checkbox" name="create_fo"></td>
+                    <td><input value="4" type="checkbox" name="delete_fo"></td>
                 </tr>
                 <tr>
-                    <td>Stock</td>
-                    <td><input type="checkbox" name="create_stock"></td>
-                    <td><input type="checkbox" name="view_stock"></td>
-                    <td><input type="checkbox" name="edit_stock"></td>
-                    <td><input type="checkbox" name="delete_stock"></td>
+                    <td>Stock Order</td>
+                    <td><select class="form-control" name="so_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="view_so"></td>
+                    <td><input value="2" type="checkbox" name="edit_so"></td>
+                    <td><input value="3" type="checkbox" name="create_so"></td>
+                    <td><input value="4" type="checkbox" name="delete_so"></td>
                 </tr>
                 <tr>
-                    <td>Waste</td>
-                    <td><input type="checkbox" name="create_waste"></td>
-                    <td><input type="checkbox" name="view_waste"></td>
-                    <td><input type="checkbox" name="edit_waste"></td>
-                    <td><input type="checkbox" name="delete_waste"></td>
+                    <td>Outdoor Order</td>
+                    <td><select class="form-control" name="odo_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="view_odo"></td>
+                    <td><input value="2" type="checkbox" name="edit_odo"></td>
+                    <td><input value="3" type="checkbox" name="create_odo"></td>
+                    <td><input value="4" type="checkbox" name="delete_odo"></td>
                 </tr>
                 <tr>
-                    <td>Products</td>
-                    <td><input type="checkbox" name="create_product"></td>
-                    <td><input type="checkbox" name="view_product"></td>
-                    <td><input type="checkbox" name="edit_product"></td>
-                    <td><input type="checkbox" name="delete_product"></td>
+                    <td>Food Catalog</td>
+                    <td><select class="form-control" name="fc_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="view_fc"></td>
+                    <td><input value="2" type="checkbox" name="edit_fc"></td>
+                    <td><input value="3" type="checkbox" name="create_fc"></td>
+                    <td><input value="4" type="checkbox" name="delete_fc"></td>
+                </tr>
+                <tr>
+                    <td>Stock Catalog</td>
+                    <td><select class="form-control" name="sc_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="view_sc"></td>
+                    <td><input value="2" type="checkbox" name="edit_sc"></td>
+                    <td><input value="3" type="checkbox" name="create_sc"></td>
+                    <td><input value="4" type="checkbox" name="delete_sc"></td>
+                </tr>
+                <tr>
+                    <td>Closing Stock</td>
+                    <td><select class="form-control" name="cs_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="view_cs"></td>
+                    <td><input value="2" type="checkbox" name="edit_cs"></td>
+                    <td><input value="3" type="checkbox" name="create_cs"></td>
+                    <td><input value="4" type="checkbox" name="delete_cs"></td>
+                </tr>
+                <tr>
+                    <td>Wastage</td>
+                    <td><select class="form-control" name="w_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="create_waste"></td>
+                    <td><input value="2" type="checkbox" name="view_waste"></td>
+                    <td><input value="3" type="checkbox" name="edit_waste"></td>
+                    <td><input value="4" type="checkbox" name="delete_waste"></td>
+                </tr>
+                <tr>
+                    <td>Counter Closing</td>
+                    <td><select class="form-control" name="cc_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td><input value="1" type="checkbox" name="create_cc"></td>
+                    <td><input value="2" type="checkbox" name="view_cc"></td>
+                    <td><input value="3" type="checkbox" name="edit_cc"></td>
+                    <td><input value="4" type="checkbox" name="delete_cc"></td>
                 </tr>
                 <tr>
                     <td>User</td>
+                    <td><select class="form-control" name="user_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
                     <td><input type="checkbox" name="create_user"></td>
                     <td><input type="checkbox" name="view_user"></td>
                     <td><input type="checkbox" name="edit_user"></td>
@@ -66,14 +120,27 @@ require('db.php');
                 </tr>
                 <tr>
                     <td>Report</td>
-                    <td><input type="checkbox" name="create_report"></td>
-                    <td><input type="checkbox" name="view_report"></td>
-                    <td><input type="checkbox" name="edit_report"></td>
-                    <td><input type="checkbox" name="delete_report"></td>
+                    <td><select class="form-control" name="r_access">
+                    <option value="0">Disable</option>
+                    <option value="1">Enable</option></select></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>Dashboard</td>
+                    <td><select class="form-control" name="d_access">
+                    <option value="1">Disable</option>
+                    <option value="0">Enable</option></select></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
                 </tr>
             </tbody>
         </table><br>
-        <button type="submit">Submit</button>
+        <button class="btn btn-primary" type="submit">Save Changes</button>
     </form>
     </div>
 </div>
